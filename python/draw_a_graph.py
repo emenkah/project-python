c1d77f7bef6a27007ea5a4f625a4db472c9f7622
Elliot Menkah <elliotsmenkah@gmail.com>
2016-09-20 16:10:39 +0200

    Program structure

---
+++ python/draw_a_graph.py
@@ -0,0 +1,22 @@
+#A  student's packages
+
+
+# B student's packages
+
+
+# B student's function
+
+def eval_func():
+
+    return X,Y,Z
+
+# A student's function
+
+def plot_func(X,Y,Z):
+
+    return
+
+# B student writes the main
+
+if __name__=="__main__":
+
