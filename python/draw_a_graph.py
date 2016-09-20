fb73932951dff5ab441789149e71c8ce979bc895
James Vance <jnvance@up.edu.ph>
2016-09-20 16:14:34 +0200

    added eval_func and some stuff

--- python/draw_a_graph.py
+++ python/draw_a_graph.py
@@ -3,10 +3,17 @@
 
 # B student's packages
 
+import numpy as np
 
 # B student's function
 
 def eval_func():
+
+    X = np.arange(-5, 5, 0.25)
+    Y = np.arange(-5, 5, 0.25)
+    X, Y = np.meshgrid(X, Y)
+    R = np.sqrt(X**2 + Y**2)
+    Z = np.sin(R)
 
     return X,Y,Z
 
@@ -20,3 +27,5 @@ def plot_func(X,Y,Z):
 
 if __name__=="__main__":
 
+    X,Y,Z = eval_func()
+    plot_func(X,Y,Z)
